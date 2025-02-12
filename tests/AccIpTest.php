3af688c00eb2dced3dc2ad4748af5df47acd2cbf
Tiago Carvalho (Tix) <tiago.carvalho@gmail.com>
2025-02-12 11:21:48 +0000

    fixed unit tests

--- tests/AccIpTest.php
+++ tests/AccIpTest.php
@@ -1,31 +1,8 @@
 <?php
 
-/*
-The MIT License (MIT)
+namespace TixAstronauta\AccIp\Tests;
 
-Copyright (c) 2015 Tiago 'Tix' Carvalho
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
- */
-
-namespace TixAstronauta\AccIp\Test;
-
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class AccIpTest extends TestCase
@@ -33,6 +10,7 @@ class AccIpTest extends TestCase
     /**
      * @dataProvider serverEnvironmentGood
      */
+    #[DataProvider('serverEnvironmentGood')]
     public function testRemoteAddr($server, $expectedIp)
     {
         $c = new \TixAstronauta\AccIp\AccIp($server);
@@ -42,6 +20,7 @@ class AccIpTest extends TestCase
     /**
      * @dataProvider serverEnvironmentBad
      */
+    #[DataProvider('serverEnvironmentBad')]
     public function testBadServerArray($server)
     {
         $c = new \TixAstronauta\AccIp\AccIp($server);
@@ -51,25 +30,21 @@ class AccIpTest extends TestCase
     /**
      * @dataProvider serverEnvironmentCustomHeaders
      */
+    #[DataProvider('serverEnvironmentCustomHeaders')]
     public function testCustomHeaders($server, $headers, $expectedIp)
     {
         $c = new \TixAstronauta\AccIp\AccIp($server, $headers);
         $this->assertEquals($expectedIp, $c->getIpAddress());
-
     }
 
     /**
      * @return array
      */
-    public function serverEnvironmentGood()
+    public static function serverEnvironmentGood()
     {
         return [
             [
-                // $server 1
-                [
-                    'REMOTE_ADDR' => '127.0.0.1'
-                ],
-                // expected output
+                ['REMOTE_ADDR' => '127.0.0.1'],
                 '127.0.0.1'
             ],
             [
@@ -88,14 +63,13 @@ class AccIpTest extends TestCase
                 ],
                 '216.58.211.238'
             ]
-
         ];
     }
 
     /**
      * @return array
      */
-    public function serverEnvironmentBad()
+    public static function serverEnvironmentBad()
     {
         return [
             [
@@ -117,7 +91,7 @@ class AccIpTest extends TestCase
     /**
      * @return array
      */
-    public function serverEnvironmentCustomHeaders()
+    public static function serverEnvironmentCustomHeaders()
     {
         return [
             [
@@ -148,6 +122,4 @@ class AccIpTest extends TestCase
             ]
         ];
     }
-
-
 }
